ca840259577b2e9647b49fb3d2d25ac2a9237098
Karuppaiya <karuppaiyacse@gmail.com>
2017-07-17 10:25:44 +0530

    random module another example program

---
+++ randomex2.py
@@ -0,0 +1,10 @@
+import random
+
+def example(num):
+    if num == 1:
+        return 'Your are Good'
+    elif num == 2:
+        return 'You are Average'
+    
+
+print(example(random.randint(1,2)))   
